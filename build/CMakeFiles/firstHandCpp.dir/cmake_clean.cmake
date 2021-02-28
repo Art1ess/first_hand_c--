320604c1f60916212bd8fe3f75f1ba0bbef7a5a2
art1ess_main <art1esspassion_main@gmail.com>
2021-02-28 22:48:56 +0800

    initialization cmake

---
+++ build/CMakeFiles/firstHandCpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstHandCpp.dir/hello_world.cpp.obj"
+  "firstHandCpp.exe"
+  "firstHandCpp.exe.manifest"
+  "firstHandCpp.pdb"
+  "libfirstHandCpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstHandCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
